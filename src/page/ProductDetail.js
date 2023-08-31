a8d9741ab2aad427371553ad734de347eff25461
ok <dhrxogus46@naver.com>
2023-08-31 21:47:08 +0900

    second

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -9,7 +9,8 @@ const ProductDetail = () => {
   const { id } = useParams();
   const getProductDetail = async () => {
     setLoading(true);
-    let url = `http://localhost:3004/products/${id}`;
+    let url = `https://my-json-server.typicode.com/oktaee
+    /ok-hnm/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     setLoading(false);
